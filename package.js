3e3a32964c91b4f6a7816244deb3d949d31c7cc6
Phil Cockfield <phil@cockfield.net>
2014-11-26 11:46:45 -0800

    Update [package.on_use] => [package.onUse]

--- package.js
+++ package.js
@@ -7,25 +7,25 @@ Package.describe({
 
 
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use(['coffeescript', 'http']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
   api.use(['respondly:css-stylus', 'respondly:ctrl', 'respondly:util']);
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('client/avatar/avatar.html', 'client');
-  api.add_files('client/spinner/spinner.html', 'client');
-  api.add_files('client/twisty/twisty.html', 'client');
-  api.add_files('client/ns.js', 'client');
-  api.add_files('client/spinner/libs/spin.js', 'client');
-  api.add_files('client/avatar/avatar.coffee', 'client');
-  api.add_files('client/avatar/avatar.styl', 'client');
-  api.add_files('client/spinner/spinner.coffee', 'client');
-  api.add_files('client/spinner/spinner.styl', 'client');
-  api.add_files('client/twisty/twisty.coffee', 'client');
-  api.add_files('client/twisty/twisty.styl', 'client');
-  api.add_files('images/avatar/anon.svg', ['client', 'server']);
-  api.add_files('images/twisty/twisty.svg', ['client', 'server']);
+  api.addFiles('client/avatar/avatar.html', 'client');
+  api.addFiles('client/spinner/spinner.html', 'client');
+  api.addFiles('client/twisty/twisty.html', 'client');
+  api.addFiles('client/ns.js', 'client');
+  api.addFiles('client/spinner/libs/spin.js', 'client');
+  api.addFiles('client/avatar/avatar.coffee', 'client');
+  api.addFiles('client/avatar/avatar.styl', 'client');
+  api.addFiles('client/spinner/spinner.coffee', 'client');
+  api.addFiles('client/spinner/spinner.styl', 'client');
+  api.addFiles('client/twisty/twisty.coffee', 'client');
+  api.addFiles('client/twisty/twisty.styl', 'client');
+  api.addFiles('images/avatar/anon.svg', ['client', 'server']);
+  api.addFiles('images/twisty/twisty.svg', ['client', 'server']);
 
 });
 
