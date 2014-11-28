1a29b3952870b3cca573eb67ed5cba19c88fd281
Phil Cockfield <phil@cockfield.net>
2014-11-27 19:07:15 -0800

    Update version constraints.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'respondly:ctrls',
   summary: 'Common UI controls',
-  version: '0.0.1',
+  version: '1.0.0',
   git: 'https://github.com/Respondly/meteor-ctrls.git'
 });
 
@@ -11,9 +11,8 @@ Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.use(['coffeescript', 'http']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
-  api.use('respondly:css-stylus');
-  api.use('respondly:ctrl');
-  api.use('respondly:util');
+  api.use('respondly:css-stylus@1.0.0');
+  api.use('respondly:ctrl@1.0.0');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
   api.addFiles('client/avatar/avatar.html', 'client');
